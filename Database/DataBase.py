11465fe9569c8c673a9b39559a7a86b0882c791d
peyflo <guilherme.peyflo@gmail.com>
2021-05-14 08:14:30 -0300

    ajustado capitalize de categorias

--- Database/DataBase.py
+++ Database/DataBase.py
@@ -20,15 +20,12 @@ class Database:
         return result_search
 
     def search_for_category(self, queries: dict):
-        if len(queries["category"]) > 1:
-            for category in range(len(queries["category"])):
-                queries["category"][category] = queries["category"][category].capitalize()
-            print(queries["category"])
-            result_search = list(self.books_collection.find({"category": {"$in": queries["category"]}}))
-            return result_search
-        else:
-            result_search = list(self.books_collection.find({"category": {"$in": queries["category"]}}))
-            return result_search
+
+        for category in range(len(queries["category"])):
+            queries["category"][category] = queries["category"][category].capitalize()
+
+        result_search = list(self.books_collection.find({"category": {"$in": queries["category"]}}))
+        return result_search
 
     def search_for_category_price(self, queries: dict):
         results = list()
